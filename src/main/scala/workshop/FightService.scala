17f93119ee4257c518b1e846c3be53f65bc5221b
Ezequiel Coronel <ezequiel.coronel@despegar.com>
2019-03-08 18:04:56 -0300

    Added suport for multiple missions

--- src/main/scala/workshop/FightService.scala
+++ src/main/scala/workshop/FightService.scala
@@ -2,12 +2,25 @@ import workshop._
 
 object FightService {
 
+  def doMissions(missions : List[Mission], team: Team) : Team = {
+    def doMission(mission: Mission, missions: List[Mission], team: Team): Team = mission.doMission(team) match {
+      case Team(List()) => Team(List())
+      case resultTeam => missions match {
+        case List() => resultTeam
+        case  _ => doMission(missions.head, missions.tail, resultTeam)
+      }
+    }
+    doMission(missions.head, missions.tail, team)
+  }
+
   def main(args: Array[String]): Unit = {
-    val team: Team = Team(List(Fighter("Yamcha", 1), Fighter("Krillin", 2000), Fighter("Goku", 5000)))
-    val mission: Mission = Mission(List(Activity(2000)))
+    val team: Team = Team(List(Fighter("Yamcha", 1), Fighter("Krillin", 2000), Fighter("Goku", 5000), Fighter("Vegeta", 4000)))
+    val mission1: Mission = Mission(List(Activity(2000)))
+    val mission2: Mission = Mission(List(Activity(200)))
+    val missions: List[Mission] = List(mission1, mission2)
 
-    val result: Team = mission.doMission(team)
-
+    println("MISION 1")
+    val result: Team = mission1.doMission(team)
     result match {
       case Team(List()) => println("team failed")
       case _ => println("team won")
@@ -18,6 +31,13 @@ object FightService {
       println(fighter.energy)
     })
 
+    println("MISION 2")
+    val result2: Team = doMissions(missions, team)
+
+    result2.fighters.foreach(fighter => {
+      println(fighter.name)
+      println(fighter.energy)
+    })
+
   }
 }
-
